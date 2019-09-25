2798c6a3b11317b3eee73a22e47fbcd265b0f082
sdawn29 <snehasishdawn29@gmail.com>
2019-09-25 15:07:40 +0530

    Update db.js

--- startup/db.js
+++ startup/db.js
@@ -1,11 +1,7 @@
 const mongoose = require('mongoose');
 
 module.exports = function() {
-    // mongoose.connect('mongodb://dawn:Mys3r3tpassword@ds143614.mlab.com:43614/codely', { useNewUrlParser: true })
-    //     .then(() => console.log('Connected to MongoDB'))
-    //     .catch(() => console.log('Error Occured'));
-
-    mongoose.connect('mongodb://localhost:27017/codely', { useNewUrlParser: true })
+    mongoose.connect(<Mongo connection String>, { useNewUrlParser: true })
         .then(() => console.log('Connected to MongoDB'))
         .catch(() => console.log('Error Occured'));
 }
